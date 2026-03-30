cfafd8e1603860d374f27f36e5e164766d76806a
Harry Franklin <hrfranklin0702@gmail.com>
2026-03-30 19:15:26 +0100

    Fixing CSS but broken npm run dev
    Page found at: https://harryfranklin.github.io/PrimeMinisterGame-React/

--- policy-sim-game/next.config.ts
+++ policy-sim-game/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: '/PrimeMinisterGame-React',
+  assetPrefix: '/PrimeMinisterGame-React/', 
 };
 
 export default nextConfig;
